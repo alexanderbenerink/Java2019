9be1ad0c90855b5123d6aee6b6e2f8a32ded2bfb
adb <39517675+alexanderbenerink@users.noreply.github.com>
2019-03-26 11:15:17 +0000

    Add files via upload

--- KlasseVB/src/KlasseApp.java
+++ KlasseVB/src/KlasseApp.java
@@ -6,14 +6,23 @@ public class KlasseApp {
 	public static void main(String[] args) {
 		Voertuig mijnFiets=new Voertuig(10.00,"auto",Color.GREEN,1200.00);
 		Voertuig jouwFiets=new Voertuig();
+		
 		boolean isGelijk=false;
 		//mijnFiets=jouwFiets;
 		//vergelijke of de inhoud hetzelfde is doe je met Equals.
+		
 		if (jouwFiets==mijnFiets){
 			isGelijk=true;
 		} else {
 			isGelijk=false;
 		}
+		
+		if (jouwFiets.equals(mijnFiets)) {
+			isGelijk=true;
+		} else {
+			isGelijk=false;
+		}
+		
 		
 		jouwFiets.setSoort("Fiets van Pietje");
 		jouwFiets.setKleur(Color.red);
